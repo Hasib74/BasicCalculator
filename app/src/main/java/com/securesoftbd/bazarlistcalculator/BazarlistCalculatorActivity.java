8bfa5a01dbd094d5bd32295353ffdb6af2a7cadf
hasib74 <hasibakon74@gmail.com>
2019-05-20 00:00:21 +0600

    This is basic calculator.

---
+++ app/src/main/java/com/securesoftbd/bazarlistcalculator/BazarlistCalculatorActivity.java
@@ -0,0 +1,64 @@
+package com.securesoftbd.bazarlistcalculator;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.securesoftbd.ssbdcalculator.SSBDCalculatorActivity;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+
+public class BazarlistCalculatorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bazarlistcalculator);
+    }
+
+
+}
+
+
+/*
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class BazarlistCalculatorActivity extends Activity {
+
+    public static final String CALCULATOR_PACKAGE = "com.android.calculator2";
+    public static final String CALCULATOR_CLASS = "com.android.calculator2.Calculator";
+
+
+    private Button showCalculator;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bazarlistcalculator);
+
+
+
+*/
+/*
+        showCalculator = (Button) findViewById(R.id.showCalculatorButton);
+
+        showCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });*//*
+
+    }
+}*/
